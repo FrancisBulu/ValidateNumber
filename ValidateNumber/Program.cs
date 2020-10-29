1aa0636868bf1ac865adfbf3ddc8057adbf7ff85
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-10-29 11:55:58 +0100

    Add project files.

---
+++ ValidateNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number pls...");
+            string input = Console.ReadLine();
+            IsNumber(input);
+            Console.WriteLine("Enter second number pls...");
+            string inputSendNumber = Console.ReadLine();
+            IsNumber(inputSendNumber);
+        }
+        static bool IsNumber(string value) // string omdat de console.Readline is altijd string
+        {
+            int number;
+            if (int.TryParse(value, out number))
+            {
+                Console.WriteLine("Valid number");
+                return true;
+                
+            }
+            else
+            {
+                Console.WriteLine("Not a valid number");
+                return false;
+            }
+        }
+    }
+}
